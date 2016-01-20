51225b3cf2c9bd623c7f338c0e07da0bf71656c3
kelle2bj <kelleybj@gmail.com>
2016-01-20 14:10:38 -0500

    Initial commit

---
+++ Twitter_DownloadProcessSentiment.py
@@ -0,0 +1,23 @@
+from pattern.web import Twitter
+from textblob import TextBlob
+
+
+t = Twitter()
+i = None
+for j in range(3):
+    for tweet in t.search('vcuResearch', start=i, count=30):
+
+        print tweet.id
+        print tweet.name
+        print tweet.text
+
+        blob = TextBlob(tweet.text)
+        for sentence in blob.sentences:
+            print(sentence.sentiment.polarity)
+
+    def fail(self, link):
+        print 'failed:', repr(link.url)
+
+#p = Polly(links=['http://www.research.vcu.edu/'], delay=1)
+#while not p.done:
+#    p.crawl(method=2, cached=False, throttle=1)
