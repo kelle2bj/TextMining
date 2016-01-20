27ebd35e5697c3d7a3ff58abe09248d4fb12878d
kelle2bj <kelleybj@gmail.com>
2016-01-20 13:33:22 -0500

    Initial commit

---
+++ crawler.py
@@ -0,0 +1,13 @@
+from pattern.web import Crawler
+
+
+class Wally(Crawler):
+    def visit(self, link, source=None):
+        print 'visited:', repr(link.url), 'from:', link.referrer
+
+    def fail(self, link):
+        print 'failed:', repr(link.url)
+
+p = Wally(links=['http://www.research.vcu.edu/'], delay=1)
+while not p.done:
+    p.crawl(method=2, cached=False, throttle=1)
